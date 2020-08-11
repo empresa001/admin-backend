6cbcd7d89911d764ad2beaffa9bbc906f136044b
David Gabriel Montaño Rocha <david.montano@cns.gob.bo>
2020-08-11 14:19:01 -0400

    Continuacion de refactory

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -54,13 +54,46 @@ const crearUsuarios = async(req, res = response) => {
 
 const actualizarUsuarios = async(req, res = response) => {
 
+    // TODO: validar token y comprobar si es usuario correcto   
+
     const uid = req.params.id;
+    // const { nombre, role, email } = req.body;
 
     try {
 
+        const usuarioDB = await Usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        // actualizacion 
+        const campos = req.body;
+
+        if (usuarioDB.email === req.body.email) {
+            delete campos.email;
+
+        } else {
+            const existeEmail = await Usuario.findOne({ email: req.body.email });
+            if (existeEmail) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese email...'
+                });
+            }
+        }
+
+        delete campos.password;
+        delete campos.google;
+
+        const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, { new: true });
+
         res.json({
             ok: true,
-            uid
+            usuario: usuarioActualizado
         });
 
     } catch (error) {
